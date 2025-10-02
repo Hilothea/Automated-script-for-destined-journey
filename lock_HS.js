f2beac7d70744427f285b1ac60589f88b9a5c753
Hilothea <suole2333@gmail.com>
2025-10-03 04:53:54 +0800

    添加好感度锁定

---
+++ lock_HS.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+    
+    function Lock_favorability(fatesystem) {
+        let favorability_S = fatesystem.希尔薇娅.好感度
+        let favorability_H = fatesystem.希洛西娅.好感度
+        if (favorability_S >= 40) {
+            avorability_S = 39
+            fatesystem.希尔薇娅.好感度 = avorability_S
+        }
+        if(favorability_H >= 40){
+            favorability_H = 39
+            fatesystem.希洛西娅.好感度 = favorability_H
+        }
+    }
+    window.Lock_favorability = Lock_favorability;
+})();
